02bcf04d6843eb8b4ba90a47d53d42c2916031b5
Samir Triande <habib.triande@gmail.com>
2020-04-21 15:46:16 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,3 +5,11 @@ def display_board(board)
   puts "-----------"
   puts " #{board[7]} | #{board[8]} | #{board[9]} "
 end
+
+def valid_move?(board, index)
+  if !position_taken?(board, index) && index.between(0,8)
+    true
+  else
+    false
+  end
+end
